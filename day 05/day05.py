136b6aab5dcb0639e23520d8314d9f6befb3349d
l3sh3 <lashadekanoidze2020@gmail.com>
2024-02-20 19:55:10 +0400

    mexute

---
+++ day 05/day05.py
@@ -0,0 +1,22 @@
+#declare varlue of first price
+first_price = 50
+
+#variable for discount
+discount = 10
+
+#declaring value of prices after 10% discount for five books
+book_1_after_discount = first_price - first_price * discount / 100
+book_2_after_discount = first_price - first_price * discount / 100
+book_3_after_discount = first_price - first_price * discount / 100
+book_4_after_discount = first_price - first_price * discount / 100
+book_5_after_discount = first_price - first_price * discount / 100
+
+#redeclaring discount to 20%
+discount = 20
+
+#declaring value of prices after 20% discount for other five books
+book_6_after_discount = first_price - first_price * discount / 100
+book_7_after_discount = first_price - first_price * discount / 100
+book_8_after_discount = first_price - first_price * discount / 100
+book_9_after_discount = first_price - first_price * discount / 100
+book_10_after_discount = first_price - first_price * discount / 100
